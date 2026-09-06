caca2dcfdcaa95bd81e57cbf96a7b4b6371bd966
Leon Koller <kevin.koller.p@gmail.com>
2026-09-06 11:55:52 -0600

    Added UI elements. Some bugs need fixing

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function GameController () {
     const player2 = createPlayer('O');
     const players = [player1, player2];
     let activePlayer = players[0];
+    let gameState = false;
 
     const handleTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -24,18 +25,28 @@ function GameController () {
     ];
 
     const playRound = (row, column) => {
+        
+
+        if(table[row][column] !== ''){
+            return;
+        }
+
+        table[row][column] = activePlayer.mark;
+
         const columnWin = checkColumnWin();
         const rowWin = checkRowWin();
         const diagWin = checkDiagonalWin();
 
         if(columnWin.win|| rowWin.win || diagWin.win){
+            players[1].giveWins();
+            gameState = true;
             return;
-        }
-
-        if(table[row][column] === ''){
-            table[row][column] = activePlayer.mark;
-            handleTurn();
-        }
+        } else if(!columnWin.win && !rowWin.win && !diagWin.win && isGameTied()){
+            gameState = true;
+            return;
+        } 
+        
+        handleTurn();
     }
 
     
@@ -79,15 +90,78 @@ function GameController () {
         }
     }
 
-    return { playRound, table };
+    const restartGame = () => {
+        table = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+        gameState = false;
+    }
+
+    const checkGameState = () => gameState;
+
+    const isGameTied = () => {
+        return !table.some(row => row.includes(''));
+    }
+
+    const getTable = () => {
+        return table;
+    }
+
+    return { playRound, getTable, checkGameState, restartGame };
 }
 
 const controller = GameController();
 
 const squares = document.querySelectorAll('.square');
+
+
+const graphicInterface = () => {
+    const resetGame = (gameState) => {
+        if(gameState){
+            const body = document.body;
+            const resetButton = document.createElement('button');
+            resetButton.innerText = 'Reset?';
+            resetButton.classList.add('reset-btn');
+
+            if(body.querySelector('.reset-btn')){
+                return;
+            }
+
+            document.body.appendChild(resetButton);
+            resetButton.addEventListener('click', () => {
+                controller.restartGame();
+                cleanTable();
+            });
+
+        }
+    }
+    const cleanTable = () => {
+        squares.forEach(square => {
+            square.innerText = '';
+        });
+    }
+
+    return { resetGame };
+}
+
+const graphicController = graphicInterface();
+
 squares.forEach(square => {
     square.addEventListener('click', () => {
-        controller.playRound(Number(square.id[0]), Number(square.id[2]))
-        square.textContent = controller.table[square.id[0]][square.id[2]];
+        const table = controller.getTable();
+        let gameState = controller.checkGameState();
+        
+        if(gameState){
+            graphicController.resetGame(gameState);
+            return;
+        } else {
+            controller.playRound(Number(square.id[0]), Number(square.id[2]));
+            square.textContent = table[square.id[0]][square.id[2]];
+        }
     });
 });
+
+
+
